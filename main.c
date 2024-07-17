2be8f2c358dc0ba09f3d2682429d1f8ec6abd51b
w <ghostoftheshell@mailbox.org>
2024-07-18 00:18:24 +0300

    Add todo items

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include <ncurses.h>
 
+#include "todo_item.h"
+
+void
+printTodoItems(WINDOW *w, TodoItem *todoItems, int len) {
+	for (int i = 0; i < len; i++) {
+		mvwprintw(w, i, 0, todoItems[i].text);
+	}
+}
+
 int main() {
 	initscr();
-	mvwprintw(stdscr, 0, 0, "TODO: TODO list");
+
+	TodoItem todoItems[] = {
+		(TodoItem) {
+			.text = "Add todo item structure",
+			.done = 1
+		},
+		(TodoItem) {
+			.text = "Add rendering with a checkbox",
+			.done = 0
+		}
+	};
+	printTodoItems(stdscr, todoItems, 2);
 	getch();
 	endwin();
 	return 0;
 }
+
+
---
+++ todo_item.h
@@ -0,0 +1,8 @@
+#ifndef TODO_ITEM_H_
+#define TODO_ITEM_H_
+typedef struct {
+	char *text;
+	int done;
+} TodoItem;
+
+#endif
