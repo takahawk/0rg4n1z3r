8c6c185e16f04907c05b953fd568f5c8a3697beb
w <ghostoftheshell@mailbox.org>
2024-07-19 23:23:24 +0300

    Implement adding new items

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/param.h>
 
 #include <ncurses.h>
@@ -6,6 +7,12 @@
 #include "todo_item.h"
 
 #define HIGHLIGHTED_PAIR 1
+
+typedef enum {
+	TODO_LIST,
+	ADD_NEW_TODO,
+	SHUTTING_DOWN
+} AppState;
 
 void
 printTodoItems(WINDOW *w, ArrayList *todoItems) {
@@ -53,6 +60,12 @@ saveTodoItemsToJSON(char *filename, ArrayList *todoItems) {
 	fclose(f);
 }
 
+AppState
+todoList(ArrayList*);
+
+AppState
+addNewTodo(ArrayList*);
+
 int main() {
 	initscr();
 
@@ -62,13 +75,38 @@ int main() {
 	}
 
 	start_color();
+	noecho();
 	init_pair(HIGHLIGHTED_PAIR, COLOR_BLACK, COLOR_WHITE);
 	keypad(stdscr, 1);
 	
+	AppState state = TODO_LIST;
+	int x, y;
+	int ch;
+
+	while (state != SHUTTING_DOWN) {
+		switch (state) {
+		case TODO_LIST:
+			state = todoList(todoItems);
+			break;
+		case ADD_NEW_TODO:
+			state = addNewTodo(todoItems);
+			break;
+		}
+	}
+	endwin();
+
+	saveTodoItemsToJSON("samples/todo.json", todoItems);
+	FreeArrayList(todoItems);
+	return 0;
+}
+
+
+AppState
+todoList(ArrayList *todoItems) {
+	wclear(stdscr);
 	int x, y;
 	int ch;
 	getyx(stdscr, y, x);
-
 	printTodoItems(stdscr, todoItems);
 	while ((ch = getch()) != 'q') {
 		switch(ch) {
@@ -89,17 +127,54 @@ int main() {
 				ArrayListSwap(todoItems, y, y - 1);
 			}
 			break;
+		case 'a':
+			return ADD_NEW_TODO;
 		}
 		y = MAX(0, (MIN(y, todoItems->len - 1)));
 		wmove(stdscr, y, x);
 		wrefresh(stdscr);
 		printTodoItems(stdscr, todoItems);
 	}
-	endwin();
 
-	saveTodoItemsToJSON("samples/todo.json", todoItems);
-	FreeArrayList(todoItems);
-	return 0;
+	return SHUTTING_DOWN;
 }
 
+AppState
+addNewTodo(ArrayList *todoItems) {
+	wclear(stdscr);
 
+	int ch;
+	int x, y;
+	int i = 0;
+	// TODO: use custom resizable buffers!
+	char* buffer = malloc(1337); 
+
+	getyx(stdscr, y, x);
+
+	mvwprintw(stdscr, y, x, "Who do you need to do: %s", buffer);
+	while ((ch = getch()) != 'q') {
+		switch(ch) {
+		case '\n':
+		case KEY_ENTER:
+			TodoItem item = {
+				.text = buffer,
+				.done = false
+			};
+			ArrayListAdd(todoItems, &item);
+			goto end;
+		case KEY_BACKSPACE:
+			if (i == 0) {
+				continue;
+			}
+			buffer[--i] = '\0';
+			break;
+		default:
+			buffer[i++] = ch;
+			buffer[i] = '\0';
+		}
+		wclear(stdscr);
+		mvwprintw(stdscr, y, x, "Who do you need to do: %s", buffer);
+	}
+end:
+	return TODO_LIST;
+}
